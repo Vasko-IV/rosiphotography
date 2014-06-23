f07fbbda07637081571a4d60edf1579aa7734252
Vasko-IV <zombaza@abv.bg>
2014-06-23 20:38:49 +0300

    Commiting

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var static = require('node-static');
+// if on OpenShift, use OpenShift ip/port, else use your local ip/port
+var ipAddress = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || 3030;
 
-var env = process.env.NODE_ENV || 'development';
+   // The rest of the code
+   :
+   :
 
-var app = express();
-var config = require('./server/config/config')[env];
 
-require('./server/config/express')(app, config);
-require('./server/config/mongoose')(config);
-require('./server/config/passport')();
-require('./server/config/routes')(app);
-
-app.listen(config.port);
-console.log("Server running on port: " + config.port);
+app.listen(port, ipAddress, function() {
+  console.log('%s: Node server started on %s:%d ...',
+                 new Date() ), ipAddress, port);
+});
